1db6861f736aa988f32e559b620c672cfb8f5fc9
Igor <igorferreiradaniel99@hotmail.com>
2022-09-27 21:36:36 -0300

    Outsorcing JPQL Queries to XML file

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repositories/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repositories/RestauranteRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +15,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 	
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 	
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
