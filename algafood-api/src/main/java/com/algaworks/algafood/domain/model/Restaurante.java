ff35c1f2d962c735cdfae5c16132ff96201c01e2
Igor Ferreira <igorferreiradaniel99@hotmail.com>
2022-09-29 03:24:45 -0300

    Mapping properties with @CreationTimestamp and @UpdateTimestamp

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +16,9 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -42,8 +46,19 @@ public class Restaurante {
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
+	@JsonIgnore
 	@Embedded
 	private Endereco endereco;
+	
+	@JsonIgnore
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCadastro;
+	
+	@JsonIgnore
+	@UpdateTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataAtualizacao;
 	
 	@JsonIgnore
 	@ManyToMany
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,10 +1,6 @@
 insert into tb_cozinha (id, nome) values (1, 'Tailandesa');
 insert into tb_cozinha (id, nome) values (2, 'Indiana');
 
-insert into tb_restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gourmet', 10, 1);
-insert into tb_restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
-insert into tb_restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
-
 insert into tb_estado (id, nome) values (1, 'Minas Gerais');
 insert into tb_estado (id, nome) values (2, 'São Paulo');
 insert into tb_estado (id, nome) values (3, 'Ceará');
@@ -14,6 +10,10 @@ insert into tb_cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
 insert into tb_cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
 insert into tb_cidade (id, nome, estado_id) values (4, 'Campinas', 2);
 insert into tb_cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
+
+insert into tb_restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, utc_timestamp, utc_timestamp, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into tb_restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp);
+insert into tb_restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (3, 'Tuk Tuk Comida Indiana', 15, 2, utc_timestamp, utc_timestamp);
 
 insert into tb_forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
 insert into tb_forma_pagamento (id, descricao) values (2, 'Cartão de débito');
