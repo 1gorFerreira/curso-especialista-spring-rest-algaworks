3a5b55f8ba2ecd9ee0d05966b34a76fd1883ad12
Igor <igorferreiradaniel99@hotmail.com>
2022-10-27 17:38:54 -0300

    MOD 8.14 - Challenge: Implementing ExceptionHandler

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,11 +8,21 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(NegocioException e){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	}
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradoException(EntidadeNaoEncontradaException e){
@@ -23,13 +33,13 @@ public class ApiExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
 	}
 	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e){
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e){
 		Problema problema = Problema.builder()
 				.dataHora(LocalDateTime.now())
 				.mensagem(e.getMessage())
 				.build();
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
 	}
 	
 	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
