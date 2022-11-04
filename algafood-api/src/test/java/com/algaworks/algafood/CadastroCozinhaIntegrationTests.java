d291c390f1af5bf1703ca89f651b0f352bd19fb0
Igor <igorferreiradaniel99@hotmail.com>
2022-11-03 21:13:00 -0300

    Mod 10.1 - 10.6

---
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,61 @@
+package com.algaworks.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.services.CadastroCozinhaService;
+
+@SpringBootTest
+public class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+	
+	@Test
+	public void testarCadastroCozinhaComSucesso() {
+		//cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		//ação
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+		
+		//validação
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
+	@Test // (expected = ConstraintViolationException.class)
+	public void testarCadastroCozinhaSemNome() {
+		Assertions.assertThrows(ConstraintViolationException.class, () -> {
+			Cozinha novaCozinha = new Cozinha();
+			novaCozinha.setNome(null);
+			
+			novaCozinha = cadastroCozinha.salvar(novaCozinha);	
+		});
+	}
+	
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
+			cadastroCozinha.excluir(1L);
+		});
+	}
+	
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
+			cadastroCozinha.excluir(1L);
+		});
+	}
+
+}
