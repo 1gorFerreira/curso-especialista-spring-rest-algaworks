153f287bcf865febb6cc4c5fc39ed4425d8ce99b
Igor <igorferreiradaniel99@hotmail.com>
2022-11-04 13:01:36 -0300

    Validating HTTP response body

--- algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -28,6 +29,21 @@ public class CadastroCozinhaIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", Matchers.hasSize(4))
+			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
 	}
 
 }
