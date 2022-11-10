4bb00278c0496b258e3c7cb324975f014eead043
Igor Ferreira <igorferreiradaniel99@hotmail.com>
2022-11-10 14:52:56 -0300

    Customizing property mapping with ModelMapper

--- algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,11 +4,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algaworks.algafood.api.model.RestauranteModel;
+import com.algaworks.algafood.domain.model.Restaurante;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		//Customizando a propriedade precoFrete dizendo que ele deve atribuir 
+		//o valor encontrado em taxaFrete do modelo de dominio, devido que o
+		//ModelMapper não conseguiu encontrar correspondencia;
+		
+		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		return modelMapper;
 	}
 }
