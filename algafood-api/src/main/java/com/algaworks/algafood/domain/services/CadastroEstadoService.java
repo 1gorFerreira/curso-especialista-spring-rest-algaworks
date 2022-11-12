83c32ce4c400b0b89fff1727dc48c31f97ea87a5
Igor <igorferreiradaniel99@hotmail.com>
2022-11-11 23:20:42 -0300

    Fixing JPA flush data exception handling bug

--- algafood-api/src/main/java/com/algaworks/algafood/domain/services/CadastroEstadoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/services/CadastroEstadoService.java
@@ -36,6 +36,7 @@ public class CadastroEstadoService {
     public void excluir(Long estadoId) {
         try {
             estadoRepository.deleteById(estadoId);
+            estadoRepository.flush();
             
         } catch (EmptyResultDataAccessException e) {
             throw new EstadoNaoEncontradoException(estadoId);
