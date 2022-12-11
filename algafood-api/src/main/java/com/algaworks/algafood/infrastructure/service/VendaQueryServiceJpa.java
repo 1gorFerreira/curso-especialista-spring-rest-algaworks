da4bb097554d98c9b9124e154d21ba82cad45484
Igor <igorferreiradaniel99@hotmail.com>
2022-12-11 11:49:19 -0300

    MOD 13.15 - Challenge: Add the filters in the daily sales query

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceJpa.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceJpa.java
@@ -1,20 +1,33 @@
 package com.algaworks.algafood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.model.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.services.VendaQueryService;
 
 @Repository
 public class VendaQueryServiceJpa implements VendaQueryService{
+	
+	/* SELECT DATE(p.data_criacao) as data_criacao,
+	 * 		count(p.id) as total_vendas,
+	 * 		sum(p.valor_total) as total_faturado
+	 * FROM pedido p
+	 * 
+	 * Predicates (Filtros);
+	 * 
+	 * group by date(p.data_criacao)
+	 */
 
 	@PersistenceContext
 	private EntityManager manager;
@@ -24,6 +37,7 @@ public class VendaQueryServiceJpa implements VendaQueryService{
 		var builder = manager.getCriteriaBuilder();
 		var query = builder.createQuery(VendaDiaria.class);
 		var root = query.from(Pedido.class);
+		var predicates = new ArrayList<Predicate>();
 		
 		// Parâmetros: Função no BD, Tipo esperado ao executar a função, argumentos
 		var functionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
@@ -33,7 +47,23 @@ public class VendaQueryServiceJpa implements VendaQueryService{
 				builder.count(root.get("id")),
 				builder.sum(root.get("valorTotal")));
 		
+		if(filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		if(filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+		}
+		
+		if (filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+		}
+		
+		predicates.add(root.get("status").in(
+				StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		
 		query.select(selection);
+		query.where(predicates.toArray(new Predicate[0]));
 		query.groupBy(functionDateDataCriacao);
 		
 		return manager.createQuery(query).getResultList(); // getResultList() Retorna um List de VendaDiaria;
