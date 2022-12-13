33b346428950159a49bbc4c64e69042db7a609bd
Igor <igorferreiradaniel99@hotmail.com>
2022-12-12 23:02:56 -0300

    Mod 14.4 - Challenge: Validating the file's content type

---
+++ algafood-api/src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.core.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+	private String[] allowedFiles;
+	
+	@Override
+	public void initialize(FileContentType constraintAnnotation) {
+		this.allowedFiles = constraintAnnotation.allowed();
+	}
+	
+	@Override
+	public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
+		for(String type : allowedFiles) {
+			if(type.equals(value.getContentType())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/*private List<String> allowedContentTypes;
+	 *
+	 * @Override
+	 * public void initialize(FileContentType constraint) {
+	 *   this.allowedContentTypes = Arrays.asList(constraint.allowed());
+	 *  }
+	 *
+	 * @Override
+	 * public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext context) {
+	 *	return multipartFile == null 
+	 *			|| this.allowedContentTypes.contains(multipartFile.getContentType());
+	 *  }
+	 */
+}
