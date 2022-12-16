6ae9108e147b92d6f16eb24aeafe07fca0ad172e
Igor <igorferreiradaniel99@hotmail.com>
2022-12-16 00:54:38 -0300

    Refatoring FotoProduto endpoint

--- algafood-api/src/main/java/com/algaworks/algafood/api/controllers/RestauranteProdutoFotoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controllers/RestauranteProdutoFotoController.java
@@ -4,17 +4,14 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.assembler.FotoProdutoModelAssembler;
 import com.algaworks.algafood.api.model.FotoProdutoModel;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
-import com.algaworks.algafood.domain.model.FotoProduto;
-import com.algaworks.algafood.domain.model.Produto;
-import com.algaworks.algafood.domain.services.CadastroProdutoService;
 import com.algaworks.algafood.domain.services.CatalogoFotoProdutoService;
 
 @RestController
@@ -22,27 +19,12 @@ import com.algaworks.algafood.domain.services.CatalogoFotoProdutoService;
 public class RestauranteProdutoFotoController {
 	
 	@Autowired
-	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
-	
-	@Autowired
-	private CadastroProdutoService cadastroProdutoService;
-	
-	@Autowired
 	private CatalogoFotoProdutoService catalogoFotoProdutoService;
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
-		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
-		
-		FotoProduto foto = new FotoProduto();
-		foto.setProduto(produto);
-		foto.setDescricao(fotoProdutoInput.getDescricao());
-		foto.setContentType(fotoProdutoInput.getArquivo().getContentType());
-		foto.setTamanho(fotoProdutoInput.getArquivo().getSize());
-		foto.setNomeArquivo(fotoProdutoInput.getArquivo().getName());
-		
-		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto);
-		return fotoProdutoModelAssembler.toModel(fotoSalva);
+	public ResponseEntity<FotoProdutoModel> atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
+		FotoProdutoModel fotoSalva = catalogoFotoProdutoService.salvar(restauranteId, produtoId, fotoProdutoInput);
+		return ResponseEntity.ok(fotoSalva);
 	}
 
 }
--- algafood-api/src/main/java/com/algaworks/algafood/domain/services/CatalogoFotoProdutoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/services/CatalogoFotoProdutoService.java
@@ -6,19 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algafood.api.assembler.FotoProdutoModelAssembler;
+import com.algaworks.algafood.api.model.FotoProdutoModel;
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
 import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.repositories.ProdutoRepository;
 
 @Service
 public class CatalogoFotoProdutoService {
 	
 	@Autowired
+	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
+	
+	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private CadastroProdutoService cadastroProdutoService;
+	
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
-		Long restauranteId = foto.getProduto().getRestaurante().getId();
-		Long produtoId = foto.getProduto().getId();
+	public FotoProdutoModel salvar(Long restauranteId, Long produtoId, FotoProdutoInput fotoProdutoInput) {
+		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
+		
+		FotoProduto foto = new FotoProduto();
+		foto.setProduto(produto);
+		foto.setDescricao(fotoProdutoInput.getDescricao());
+		foto.setContentType(fotoProdutoInput.getArquivo().getContentType());
+		foto.setTamanho(fotoProdutoInput.getArquivo().getSize());
+		foto.setNomeArquivo(fotoProdutoInput.getArquivo().getName());
 		
 		Optional<FotoProduto> fotoExistente = produtoRepository.findFotoById(restauranteId, produtoId);
 		
@@ -26,7 +42,7 @@ public class CatalogoFotoProdutoService {
 			produtoRepository.delete(fotoExistente.get());
 		}
 		
-		return produtoRepository.save(foto);
+		return fotoProdutoModelAssembler.toModel(produtoRepository.save(foto));
 	}
 	
 }
