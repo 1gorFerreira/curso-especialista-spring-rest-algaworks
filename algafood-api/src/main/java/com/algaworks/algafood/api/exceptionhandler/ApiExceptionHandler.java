89db7edd1f398a1e0662473c5641ed7752a38cbf
Igor <igorferreiradaniel99@hotmail.com>
2022-12-21 01:34:02 -0300

    Fixing exception handler media type not accepted

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -194,6 +195,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 				.build();
 		
 		return super.handleExceptionInternal(ex, problem, headers, status, request);
+	}
+	
+	//Customizando exception handlers de ResponseEntityExceptionHandler:
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(status).headers(headers).build();
 	}
 	
 	//Tratando qualquer outra excecao que ocorrer;
