c233859b58e3647d42f788a8081b4e1f2656e7ae
Igor <igorferreiradaniel99@hotmail.com>
2022-12-21 16:13:15 -0300

    Defining Amazon S3 Client Bean and Configuring Credentials

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,13 +2,18 @@ package com.algaworks.algafood.infrastructure.service.storage;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.services.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService{
 
+	@Autowired
+	private AmazonS3 amazonS3;
+	
 	@Override
 	public InputStream recuperar(String nomeArquivo) {
 		return null;
