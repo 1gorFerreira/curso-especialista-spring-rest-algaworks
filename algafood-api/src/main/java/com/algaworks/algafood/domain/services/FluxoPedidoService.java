d81760812cba856bdc94b0c7ae0b22cd6f7b6498
Igor <igorferreiradaniel99@hotmail.com>
2022-12-23 16:29:56 -0300

    Using email sender service in order confirmation

--- algafood-api/src/main/java/com/algaworks/algafood/domain/services/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/services/FluxoPedidoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.services.EnvioEmailService.Mensagem;
 
 @Service
 public class FluxoPedidoService {
@@ -12,10 +13,21 @@ public class FluxoPedidoService {
 	@Autowired
 	private EmissaoPedidoService cadastroPedidoService;
 	
+	@Autowired
+	private EnvioEmailService envioEmailService;
+	
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = cadastroPedidoService.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+		
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+		
+		envioEmailService.enviar(mensagem);
 	}
 	
 	@Transactional
