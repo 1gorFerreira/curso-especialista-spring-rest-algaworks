39c11671c60276742eda43ada011a2a254ca0209
Igor <igorferreiradaniel99@hotmail.com>
2023-01-17 20:10:27 -0300

    Describing response status codes globally

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,14 +1,20 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,8 +31,23 @@ public class SpringFoxConfig {
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 					.build()
+				.useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"));
+	}
+	
+	private List<Response> globalGetResponseMessages(){
+		return Arrays.asList(
+				new ResponseBuilder()
+					.code("500")
+					.description("Erro interno do servidor")
+					.build(),
+				new ResponseBuilder() 
+					.code("406")
+					.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+					.build()
+			);
 	}
 	
 	public ApiInfo apiInfo() {
