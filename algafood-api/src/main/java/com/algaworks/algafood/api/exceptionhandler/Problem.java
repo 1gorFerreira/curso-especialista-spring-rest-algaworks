ceaa19628167bc29a5d226de4478eb4413563caf
Igor <igorferreiradaniel99@hotmail.com>
2023-01-17 22:46:28 -0300

    Describing the problem representation model

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -6,28 +6,47 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+@ApiModel("Problema")
 @JsonInclude(Include.NON_NULL)//Só inclua na representação valores que não sejam NULL;
 @Getter
 @Builder
 public class Problem {
 	
+	@ApiModelProperty(example = "400")
 	private Integer status;
+	
+	@ApiModelProperty(example = "https://algafood.com.br/dados-invalidos")
 	private String type;
+	
+	@ApiModelProperty(example = "Dados inválidos")
 	private String title;
+	
+	@ApiModelProperty(example = "2023-01-18T01:26:03.1355129Z")
 	private OffsetDateTime timestamp;
+	
+	@ApiModelProperty(value = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
 	private String detail;
 	
+	@ApiModelProperty(value = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
 	private String userMessage;
+	
+	@ApiModelProperty(value = "Lista de objetos ou campos que geraram o erro (opcional)")
 	private List<Object> objects;
 	
+	@ApiModel("ObjetoProblema")
 	@Getter
 	@Builder
 	public static class Object {
 		
+		@ApiModelProperty(example = "preco")
 		private String name;
+		
+		@ApiModelProperty(example = "O preço é obrigatório")
 		private String userMessage;
 		
 	}
--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -9,6 +9,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,6 +22,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -28,6 +33,8 @@ public class SpringFoxConfig {
 
 	@Bean
 	public Docket apiDocket() {
+		TypeResolver typeResolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.OAS_30)
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
@@ -37,6 +44,7 @@ public class SpringFoxConfig {
 				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
 				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
 				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+				.additionalModels(typeResolver.resolve(Problem.class))
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"));
 	}
@@ -95,6 +103,12 @@ public class SpringFoxConfig {
 				.version("1")
 				.contact(new Contact("Igor", "https://github.com/1gorFerreira", "igorferreiradaniel99@hotmail.com"))
 				.build();
+	}
+	
+	//Resolvendo problema da aula 18.14;
+	@Bean
+	public JacksonModuleRegistrar springFoxJacksonConfig() {
+		return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
 	}
 	
 }
