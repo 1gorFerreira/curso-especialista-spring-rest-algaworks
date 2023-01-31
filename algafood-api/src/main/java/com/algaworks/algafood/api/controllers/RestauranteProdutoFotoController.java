1018e6ba3961881dad547f739736b3499292ac61
Igor <igorferreiradaniel99@hotmail.com>
2023-01-30 22:09:29 -0300

    Mod 18.35 - Challenge: describing product photo endpoint documentation

--- algafood-api/src/main/java/com/algaworks/algafood/api/controllers/RestauranteProdutoFotoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controllers/RestauranteProdutoFotoController.java
@@ -21,24 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.model.FotoProdutoModel;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.api.openapi.controller.RestauranteProdutoFotoControllerOpenApi;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.services.CatalogoFotoProdutoService;
 import com.algaworks.algafood.domain.services.FotoStorageService.FotoRecuperada;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
-public class RestauranteProdutoFotoController {
+public class RestauranteProdutoFotoController implements RestauranteProdutoFotoControllerOpenApi{
 	
 	@Autowired
 	private CatalogoFotoProdutoService catalogoFotoProdutoService;
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping
 	public ResponseEntity<FotoProdutoModel> buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId){
 		FotoProdutoModel foto = catalogoFotoProdutoService.buscar(restauranteId, produtoId);
 		return ResponseEntity.ok(foto);
 	}
 	
-	@GetMapping // (produces = { MediaType.IMAGE_JPEG_VALUE , MediaType.IMAGE_PNG_VALUE }) -> Implementado em verificarCompatibilidadeMediaType;
+	@GetMapping(produces = MediaType.ALL_VALUE) // (produces = { MediaType.IMAGE_JPEG_VALUE , MediaType.IMAGE_PNG_VALUE }) -> Implementado em verificarCompatibilidadeMediaType;
 	public ResponseEntity<?> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
 			@RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException{
 		try {
@@ -73,7 +74,7 @@ public class RestauranteProdutoFotoController {
 	}
 	
 	@DeleteMapping
-	public ResponseEntity<Void> delete(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+	public ResponseEntity<Void> excluir(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
 		catalogoFotoProdutoService.deletar(restauranteId, produtoId);
 		return ResponseEntity.noContent().build();
 	}
---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -0,0 +1,66 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.FotoProdutoModel;
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@Api(tags = "Produtos")
+public interface RestauranteProdutoFotoControllerOpenApi {
+
+    @ApiOperation("Atualiza a foto do produto de um restaurante")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Foto do produto atualizada"),
+        @ApiResponse(responseCode = "404", description = "Produto de restaurante não encontrado", content = @Content(schema = @Schema(implementation = Problem.class)))
+    })
+    ResponseEntity<FotoProdutoModel> atualizarFoto(
+            @ApiParam(value = "ID do restaurante", example = "1", required = true)
+            Long restauranteId,
+            
+            @ApiParam(value = "ID do produto", example = "1", required = true)
+            Long produtoId,
+            
+            FotoProdutoInput fotoProdutoInput) throws IOException;
+
+    @ApiOperation("Exclui a foto do produto de um restaurante")
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "Foto do produto excluída"),
+        @ApiResponse(responseCode = "400", description = "ID do restaurante ou produto inválido", content = @Content(schema = @Schema(implementation = Problem.class))),
+        @ApiResponse(responseCode = "404", description = "Foto de produto não encontrada", content = @Content(schema = @Schema(implementation = Problem.class)))
+    })
+    ResponseEntity<Void> excluir(
+            @ApiParam(value = "ID do restaurante", example = "1", required = true)
+            Long restauranteId,
+            
+            @ApiParam(value = "ID do produto", example = "1", required = true)
+            Long produtoId);
+
+    @ApiOperation(value = "Busca a foto do produto de um restaurante",
+            produces = "application/json, image/jpeg, image/png")
+    @ApiResponses({
+        @ApiResponse(responseCode = "400", description = "ID do restaurante ou produto inválido", content = @Content(schema = @Schema(implementation = Problem.class))),
+        @ApiResponse(responseCode = "404", description = "Foto de produto não encontrada", content = @Content(schema = @Schema(implementation = Problem.class)))
+    })
+    ResponseEntity<FotoProdutoModel> buscar(
+            @ApiParam(value = "ID do restaurante", example = "1", required = true)
+            Long restauranteId,
+            
+            @ApiParam(value = "ID do produto", example = "1", required = true)
+            Long produtoId);
+
+    @ApiOperation(value = "Busca a foto do produto de um restaurante", hidden = true)
+    ResponseEntity<?> servirFoto(Long restauranteId, Long produtoId, String acceptHeader) 
+            throws HttpMediaTypeNotAcceptableException;
+}
