fe6fc1ca83e6949c9758d10c10836b60aba29a5d
Igor <igorferreiradaniel99@hotmail.com>
2023-02-02 21:53:54 -0300

    Building links that point to methods

--- algafood-api/src/main/java/com/algaworks/algafood/api/controllers/CidadeController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controllers/CidadeController.java
@@ -62,14 +62,23 @@ public class CidadeController implements CidadeControllerOpenApi{
 //		cidadeModel.add(Link.of("http://api.algafood.local:8080/cidades", IanaLinkRelations.COLLECTION));
 //		cidadeModel.getEstado().add(Link.of("http://api.algafood.local:8080/estados/1"));
 		
-		cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class)
-				.slash(cidadeModel.getId()).withSelfRel());
+//		cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class)
+//				.slash(cidadeModel.getId()).withSelfRel());
+//		
+//		cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class)
+//				.withRel("cidades"));
+//		
+//		cidadeModel.getEstado().add(WebMvcLinkBuilder.linkTo(EstadoController.class)
+//				.slash(cidadeModel.getEstado().getId()).withSelfRel());
 		
-		cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class)
-				.withRel("cidades"));
+		cidadeModel.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CidadeController.class)
+				.buscar(cidadeModel.getId())).withSelfRel());
 		
-		cidadeModel.getEstado().add(WebMvcLinkBuilder.linkTo(EstadoController.class)
-				.slash(cidadeModel.getEstado().getId()).withSelfRel());
+		cidadeModel.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CidadeController.class)
+				.listar()).withRel("cidades"));
+		
+		cidadeModel.getEstado().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EstadoController.class)
+				.buscar(cidadeModel.getEstado().getId())).withSelfRel());
 		
 		return ResponseEntity.ok(cidadeModel);
 	}
