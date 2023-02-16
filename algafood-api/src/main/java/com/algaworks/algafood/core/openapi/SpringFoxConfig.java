778d9b43ebee1dcc0c5e5874d4a8612e25b6e729
Igor <igorferreiradaniel99@hotmail.com>
2023-02-16 15:07:23 -0300

    Deprecating an API version

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -247,8 +247,11 @@ public class SpringFoxConfig {
 	
 	public ApiInfo apiInfoV1() {
 		return new ApiInfoBuilder()
-				.title("AlgaFood API")
-				.description("API aberta para clientes e restaurantes")
+				.title("AlgaFood API (Depreciada)")
+//				.description("API aberta para clientes e restaurantes")
+				.description("API aberta para clientes e restaurantes.<br>"
+						+ "<strong>Essa versão da API está depreciada e deixará de existir a partir de 01/01/2021. "
+						+ "Use a versão mais atual da API.")
 				.version("1")
 				.contact(new Contact("Igor", "https://github.com/1gorFerreira", "igorferreiradaniel99@hotmail.com"))
 				.build();
