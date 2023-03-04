4dc6d6eabe213da2c30662bca7ef05633360d642
Igor <igorferreiradaniel99@hotmail.com>
2023-03-04 19:30:45 -0300

    Generating JWT with symmetric key (HMAC SHA-256) in Authorization Server

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -3,8 +3,8 @@ package com.algaworks.algafood.auth;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,8 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -30,9 +29,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
-	@Autowired
-	private RedisConnectionFactory redisConnectionFactory;
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -67,20 +63,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient("checktoken") // Acesso proprio do ResourceServer para autenticar no AuthorizationServer;
 			.secret(passwordEncoder.encode("check123"));
 	}
-
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints
-			.authenticationManager(authenticationManager)
-			.userDetailsService(userDetailsService)
-			.reuseRefreshTokens(false)
-			.tokenStore(redisTokenStore())
-			.tokenGranter(tokenGranter(endpoints));
-	}
-	
-	private TokenStore redisTokenStore() {
-		return new RedisTokenStore(redisConnectionFactory);
-	}
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -90,7 +72,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 // Ao inves de passar as credenciais do cliente apenas como HTTPBasic no Authorization, passaremos uma chave no corpo da req (x-www-form/form-data);
 			.allowFormAuthenticationForClients();
 	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService)
+			.reuseRefreshTokens(false)
+			.accessTokenConverter(jwtAccessTokenConverter())
+			.tokenGranter(tokenGranter(endpoints));
+	}
 	
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("algaworks");
+		
+		return jwtAccessTokenConverter;
+	}
 	
 	// Parte da configuração do Authorization Server, para adicionar o TokenGranter customizado (PKCE)
 	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
