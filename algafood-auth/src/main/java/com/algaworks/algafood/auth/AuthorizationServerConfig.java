c029d34edc6945d4223a8c0fd0fcbcd3ecf640d0
Igor <igorferreiradaniel99@hotmail.com>
2023-03-04 22:15:34 -0300

    Signing the JWT with RSA SHA-256 (asymmetric key)

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
@@ -86,7 +88,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey("89asjkdhiuhep121289u3hdpasuighd1he9");
+//		jwtAccessTokenConverter.setSigningKey("89asjkdhiuhep121289u3hdpasuighd1he9"); Chave simetrica
+
+		var jksResource = new ClassPathResource("keystores/algafood.jks");
+		var keyStorePass = "123456";
+		var keyPairAlias = "algafood";
+		
+		var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+		var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+		
+		jwtAccessTokenConverter.setKeyPair(keyPair); // Chave assimetrica
 		
 		return jwtAccessTokenConverter;
 	}
