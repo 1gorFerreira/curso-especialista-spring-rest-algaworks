563f0d926733e46f99b4bfabb237b706e9fd2a4a
Igor <igorferreiradaniel99@hotmail.com>
2023-03-07 16:41:09 -0300

    Adding custom claims in the JWT payload

---
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/core/JwtCustomClaimsTokenEnhancer.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.auth.core;
+
+import java.util.HashMap;
+
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+public class JwtCustomClaimsTokenEnhancer implements TokenEnhancer{
+
+	@Override
+	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+		if(authentication.getPrincipal() instanceof AuthUser) { //Verificando se o fluxo do OAuth2 em uso utiliza autenticacao do usuario final;
+			var authUser = (AuthUser) authentication.getPrincipal();
+			
+			//Se utiliza, iremos acrescentar informacoes ao Token:
+			var info = new HashMap<String, Object>();
+			info.put("nome_completo", authUser.getFullName());
+			info.put("usuario_id", authUser.getUserId());
+			
+			//Downcast para acessar o metodo setAdditional... para adicionarmos as informacoes anteriores ao token;
+			var oAuth2AccessToken = (DefaultOAuth2AccessToken) accessToken;
+			oAuth2AccessToken.setAdditionalInformation(info);
+			
+		}
+		
+		return accessToken;
+	}
+
+	
+}
