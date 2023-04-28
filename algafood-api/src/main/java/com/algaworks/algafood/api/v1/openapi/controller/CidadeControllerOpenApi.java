8e69c38dea9f1da154698e8efce6c85d09a7a6ce
Igor <igorferreiradaniel99@hotmail.com>
2023-04-28 11:27:11 -0300

    Describing endpoint operations in the documentation

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.openapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -12,13 +13,19 @@ import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 @Tag(name = "Cidades", description = "Gerencia as cidades")
 public interface CidadeControllerOpenApi {
 
+	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeModel> listar();
 
+	@Operation(summary = "Busca uma cidade por id")
 	ResponseEntity<CidadeModel> buscar(Long cidadeId);
 
+	@Operation(summary = "Cadastra uma cidade",
+			description = "Cadastro de uma cidade, necessita de um estado e um nome valido")
 	ResponseEntity<CidadeModel> adicionar(CidadeInput cidadeInput);
 
+	@Operation(summary = "Atualizar uma cidade por ID")
 	ResponseEntity<CidadeModel> atualizar(Long id, CidadeInput cidadeInput);
 
+	@Operation(summary = "Excluir uma cidade por ID")
 	ResponseEntity<?> remover(Long cidadeId);
 }
