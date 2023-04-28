6c741d0eb2a0e5837827f84f933391996c9d9335
Igor <igorferreiradaniel99@hotmail.com>
2023-04-28 11:18:58 -0300

    Describing tags in documentation and associating with controllers

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.v1.openapi.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
@@ -8,6 +9,7 @@ import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Cidades", description = "Gerencia as cidades")
 public interface CidadeControllerOpenApi {
 
 	CollectionModel<CidadeModel> listar();
--- algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -8,8 +8,11 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 @SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
@@ -34,7 +37,9 @@ public class SpringDocConfig {
                                 .name("Apache 2.0")
                                 .url("http://springdoc.com")
                         )
-                );
+                ).tags(Arrays.asList(
+                        new Tag().name("Cidades").description("Gerencia as cidades")
+                ));
     }
 
 }
