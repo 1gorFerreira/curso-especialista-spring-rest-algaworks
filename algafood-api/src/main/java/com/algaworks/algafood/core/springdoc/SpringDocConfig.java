aef0a4ed9bcca4cd8d937a999a4103ab4774ab64
Igor <igorferreiradaniel99@hotmail.com>
2023-05-08 20:51:37 -0300

    Describing response status codes globally

--- algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -8,7 +8,10 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,6 +43,27 @@ public class SpringDocConfig {
                 ).tags(Arrays.asList(
                         new Tag().name("Cidades").description("Gerencia as cidades")
                 ));
+    }
+
+    @Bean
+    public OpenApiCustomiser openApiCustomiser(){
+        return openApi -> {
+            openApi.getPaths()
+                    .values()
+                    .stream()
+                    .flatMap(pathItem -> pathItem.readOperations().stream())
+                    .forEach(operation -> {
+                        ApiResponses responses = operation.getResponses();
+
+                        ApiResponse apiResponseNaoEncontrado = new ApiResponse().description("Recurso nao encontrado");
+                        ApiResponse apiResponseErroInterno = new ApiResponse().description("Erro interno no servidor");
+                        ApiResponse apiResponseSemRepresentacao = new ApiResponse().description("Recurso nao possui uma representacao que poderia ser aceita pelo consumidor");
+
+                        responses.addApiResponse("500", apiResponseErroInterno);
+                        responses.addApiResponse("404", apiResponseNaoEncontrado);
+                        responses.addApiResponse("406", apiResponseSemRepresentacao);
+            });
+        };
     }
 
 }
