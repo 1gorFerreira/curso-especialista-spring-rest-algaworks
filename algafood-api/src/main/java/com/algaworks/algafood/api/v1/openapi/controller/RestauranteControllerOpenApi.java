aec6ec78d7a45760b763a12e5a8e93bb0f0c372b
Igor <igorferreiradaniel99@hotmail.com>
2023-05-12 15:04:27 -0300

    Describing projection parameters in query endpoints

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
@@ -2,7 +2,11 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.data.repository.query.Param;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
@@ -15,8 +19,15 @@ import com.algaworks.algafood.api.v1.model.input.RestauranteInput;
 public interface RestauranteControllerOpenApi {
 
 //  @JsonView(RestauranteView.Resumo.class)
+	@Operation(parameters = {
+		@Parameter(name = "projecao", description = "Nome da projecao",
+				example = "apenas-nome",
+				in = ParameterIn.QUERY,
+				required = false)
+	})
 	CollectionModel<RestauranteBasicoModel> listar();
 
+	@Operation(hidden = true)
 	CollectionModel<RestauranteApenasNomeModel> listarApenasNomes();
 
 	ResponseEntity<RestauranteModel> buscar(Long restauranteId);
