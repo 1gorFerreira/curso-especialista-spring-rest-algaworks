d4798fe424e06b5307df0b1ee93b98b5381e7a73
Igor <igorferreiradaniel99@hotmail.com>
2023-05-15 20:09:05 -0300

    Mod 26.31 - Challenge: Describing documentation of associating permissions with groups endpoints

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/model/PermissaoModel.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/model/PermissaoModel.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
@@ -11,10 +12,13 @@ import lombok.Setter;
 @Setter
 public class PermissaoModel extends RepresentationModel<PermissaoModel>{
 
+	@Schema(example = "1")
 	private Long id;
-	
+
+	@Schema(example = "CONSULTAR_COZINHAS")
 	private String nome;
-	
+
+	@Schema(example = "Permite consultar cozinhas")
 	private String descricao;
 	
 }
