51d0ded8d1474a8949103d50cfae7a9feea4a8e0
Igor <igorferreiradaniel99@hotmail.com>
2023-05-15 20:55:35 -0300

    Mod 26.35 - Challenge: describing permissions endpoint documentation

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/PermissaoControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/PermissaoControllerOpenApi.java
@@ -1,13 +1,17 @@
 package com.algaworks.algafood.api.v1.openapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 
 import com.algaworks.algafood.api.v1.model.PermissaoModel;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Permissões")
 public interface PermissaoControllerOpenApi {
 
+    @Operation(summary = "Lista as permissões")
     CollectionModel<PermissaoModel> listar();
     
 }
