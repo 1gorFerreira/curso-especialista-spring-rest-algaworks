0010fddf7694ed17bcc38a40305fe89ae2530fde
Igor <igorferreiradaniel99@hotmail.com>
2023-05-15 20:24:13 -0300

    Mod 26.32 - Challenge: describing user endpoint documentation

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/model/input/UsuarioInput.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/model/input/UsuarioInput.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.v1.model.input;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +11,11 @@ import lombok.Setter;
 @Setter
 public class UsuarioInput {
 
+	@Schema(example = "João da Silva")
 	@NotBlank
 	private String nome;
-	
+
+	@Schema(example = "joao.ger@algafood.com.br")
 	@NotBlank
 	@Email
 	private String email;
--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
@@ -1,6 +1,13 @@
 package com.algaworks.algafood.api.v1.openapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
@@ -10,15 +17,43 @@ import com.algaworks.algafood.api.v1.model.input.UsuarioComSenhaInput;
 import com.algaworks.algafood.api.v1.model.input.UsuarioInput;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Usuários")
 public interface UsuarioControllerOpenApi {
 
+	@Operation(summary = "Lista os usuários")
 	ResponseEntity<CollectionModel<UsuarioModel>> buscarTodos();
 
-	ResponseEntity<UsuarioModel> buscar(Long usuarioId);
+	@Operation(summary = "Busca um usuário por ID",
+			responses = {
+				@ApiResponse(responseCode = "200"),
+				@ApiResponse(responseCode = "400", description = "ID do usuário inválido", content = {
+						@Content(schema = @Schema(ref = "Problema")) }),
+				@ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = {
+						@Content(schema = @Schema(ref = "Problema")) }),
+	})
+	ResponseEntity<UsuarioModel> buscar(@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId);
 
-	ResponseEntity<UsuarioModel> adicionar(UsuarioComSenhaInput usuarioInput);
+	@Operation(summary = "Cadastra um usuário",
+			responses = {
+				@ApiResponse(responseCode = "201", description = "Usuário cadastrado"),
+	})
+	ResponseEntity<UsuarioModel> adicionar(@RequestBody(description = "Representação de um novo usuário", required = true) UsuarioComSenhaInput usuarioInput);
 
-	ResponseEntity<UsuarioModel> atualizar(Long usuarioId, UsuarioInput usuarioInput);
+	@Operation(summary = "Atualiza um usuário por ID",
+			responses = {
+				@ApiResponse(responseCode = "200", description = "Usuário atualizado"),
+				@ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = {
+						@Content(schema = @Schema(ref = "Problema")) })
+	})
+	ResponseEntity<UsuarioModel> atualizar(@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId,
+										   @RequestBody(description = "Representação de um usuário com os novos dados", required = true) UsuarioInput usuarioInput);
 
-	ResponseEntity<Void> atualizarSenha(Long usuarioId, SenhaInput senha);
+	@Operation(summary = "Atualiza a senha de um usuário",
+			responses = {
+				@ApiResponse(responseCode = "204", description = "Senha alterada com sucesso"),
+				@ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = {
+						@Content(schema = @Schema(ref = "Problema")) })
+	})
+	ResponseEntity<Void> atualizarSenha(@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId,
+										@RequestBody(description = "Representação de uma nova senha", required = true) SenhaInput senha);
 }
