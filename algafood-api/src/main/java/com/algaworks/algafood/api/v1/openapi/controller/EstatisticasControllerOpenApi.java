38578b6a00d485a84890bcf6e01de92899805438
Igor <igorferreiradaniel99@hotmail.com>
2023-05-15 20:51:43 -0300

    Mod 26.34 - Challenge: describing statistics endpoint documentation

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
@@ -2,7 +2,15 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.v1.controllers.EstatisticasController.EstatisticasModel;
@@ -10,11 +18,29 @@ import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.model.filter.VendaDiariaFilter;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Estatísticas")
 public interface EstatisticasControllerOpenApi {
 
-	List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
+	@Operation(
+			summary = "Consulta estatísticas de vendas diárias",
+			parameters = {
+					@Parameter(in = ParameterIn.QUERY, name = "restauranteId", description = "ID do restaurante", example = "1", schema = @Schema(type = "integer")),
+					@Parameter(in = ParameterIn.QUERY, name = "dataCriacaoInicio", description = "Data/hora inicial da criação do pedido", example = "2019-12-01T00:00:00Z", schema = @Schema(type = "string", format = "date-time")),
+					@Parameter(in = ParameterIn.QUERY, name = "dataCriacaoFim", description = "Data/hora final da criação do pedido", example = "2019-12-02T23:59:59Z", schema = @Schema(type = "string", format = "date-time"))
+			},
+			responses = {
+					@ApiResponse(responseCode = "200", content = {
+							@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = VendaDiaria.class))),
+							@Content(mediaType = "application/pdf", schema = @Schema(type = "string", format = "binary")),
+					}),
+			}
+	)
+	List<VendaDiaria> consultarVendasDiarias(@Parameter(hidden = true) VendaDiariaFilter filtro,
+											 @Parameter(description = "Deslocamento de horário a ser considerado na consulta em relação ao UTC", schema = @Schema(type = "string", defaultValue = "+00:00")) String timeOffset);
 
+	@Operation(hidden = true)
 	ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro, String timeOffset);
 
+	@Operation(hidden = true)
 	EstatisticasModel estatisticas();
 }
